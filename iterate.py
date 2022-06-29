dbca561efe76eb80a4a297c20bf8122e68366a5d
Mariam Hergnyan <56159919+mariamhergnyan@users.noreply.github.com>
2022-06-29 15:03:28 +0200

    Update iterate.py

--- iterate.py
+++ iterate.py
@@ -2,4 +2,4 @@ import os
 import run_3rd.py #have these scripts in the same folder
 for i in range(0,11):
         os.system("run_3rd.py")  
-	runipy third_analysis.ipynb output.ipynyb
+	runipy third_analysis.ipynb output.ipynb
